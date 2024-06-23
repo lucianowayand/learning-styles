bb45e80c6dcf842957ff5edbbfd5be0aae2c88f4
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-06-23 16:56:40 -0300

    fix: removing core entity

--- src/modules/models/model.entity.ts
+++ src/modules/models/model.entity.ts
@@ -1,8 +1,19 @@
-import { CoreEntity } from 'src/core/core.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('models')
-export class ModelEntity extends CoreEntity {
+export class ModelEntity {
+  @PrimaryColumn('uuid', { default: () => 'gen_random_uuid()' })
+  id: string;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column('timestamp', { nullable: true, default: null })
+  deletedAt: Date;
+
   @Column()
   name: string;
 
--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,8 +1,19 @@
-import { CoreEntity } from 'src/core/core.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('users')
-export class UserEntity extends CoreEntity {
+export class UserEntity {
+  @PrimaryColumn('uuid', { default: () => 'gen_random_uuid()' })
+  id: string;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column('timestamp', { nullable: true, default: null })
+  deletedAt: Date;
+
   @Column()
   name: string;
 
