cc946021bb662bd1c6aef9baf10a14f21d93e4fe
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-06-23 20:58:52 -0300

    :(

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,6 @@ async function bootstrap() {
 
   app.enableCors();
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
   await app.listen(3333);
 }
 bootstrap();
