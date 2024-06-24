d9181890d6b1dac6247ac53b71d9a2fa6d679b45
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-06-24 00:59:57 -0300

    refactor: sorting resolutions

--- src/modules/resolutions/resolutions.service.ts
+++ src/modules/resolutions/resolutions.service.ts
@@ -28,6 +28,9 @@ export class ResolutionsService {
         userId,
       },
       relations: ['learningType'],
+      order: {
+        createdAt: 'DESC',
+      },
     });
 
     if (modelId) {
